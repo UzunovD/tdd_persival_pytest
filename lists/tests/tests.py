dfd43a98d692cae806fc4df603cf68b5530d684c
Uzunov Dmitry <duyz@ya.ru>
2022-08-14 15:03:28 +0300

    Рефакторинг тестирования домашнией страницы с помошью шаблона

--- lists/tests/tests.py
+++ lists/tests/tests.py
@@ -1,11 +1,8 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists import views
 
 
-class Test:
+class HomePageTest(TestCase):
     def test_home_page_return_correct_html(self):
-        """Проверяет корректность ответа home_page"""
-        request = HttpRequest()
-        html = views.home_page(request).content.decode('utf-8')
-        assert '<html><title>To-Do lists</title></html>' in html
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
